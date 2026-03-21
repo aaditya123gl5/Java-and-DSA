5d1052e70480621c9877592a7120f7db9fe4cff7
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-03-21 19:14:19 +0530

    Nice

---
+++ basics/BasicClass.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class BasicClass {
+    private String title;
+    public BasicClass(String title){
+        this.title=title;
+    }
+    public void watchMovie(){
+        String type=this.getClass().getSimpleName();
+        System.out.println(type);
+    }
+    public static void main(){
+        BasicClass b=new BasicClass("Bahubali");
+        b.watchMovie();
+    }
+    
+}
