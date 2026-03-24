9a09782cb55e2e7f3853125111bfb88c3f81c411
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-03-24 16:31:20 +0530

    Second largest

---
+++ Array/SecondLargest.java
@@ -0,0 +1,13 @@
+package Array;
+import java.util.*;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        
+    Scanner scn=new Scanner(System.in);
+    int[] arr={4,6,2,7,9};
+    Arrays.sort(arr);
+    int l=arr.length;
+    System.out.println(arr[l-2]);
+}
+}
