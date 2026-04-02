f9dd1d3622bba45e78e7e162262fbfd9139e8df4
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-04-02 13:59:47 +0530

    prime

---
+++ basics/PrimeNo.java
@@ -0,0 +1,24 @@
+package basics;
+import java.util.*;
+
+public class PrimeNo {
+    public static void main(String[]args){
+
+    Scanner scn=new Scanner(System.in);
+    int num=scn.nextInt();
+    int count=0;
+    for(int i=2;i<=num;i++){
+        if(num%i==0){
+            count++;
+        }
+    }
+    if(count==1){
+        System.out.println("Prime number");
+    }
+    else{
+        System.out.println("Not Prime number");
+    }
+}
+}
+
+    
