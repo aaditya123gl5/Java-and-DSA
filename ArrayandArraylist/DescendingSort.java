134012dbe8a3f25ce2428a2a47a7df51fb3ba802
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-04-08 15:46:30 +0530

    Descending sort is the way

---
+++ ArrayandArraylist/DescendingSort.java
@@ -0,0 +1,28 @@
+package ArrayandArraylist;
+import java.util.Arrays;
+import java.util.Random;
+
+public class DescendingSort {
+  public static void main(String[]args){
+    Random rd=new Random();
+    int[] arr=new int[5];
+    for(int i=0;i<arr.length;i++){
+        arr[i]=rd.nextInt(50);
+    }
+    System.out.println("Before Sorting:"+Arrays.toString(arr));
+    for(int i=0;i<arr.length-1;i++){
+        for(int j=0;j<arr.length-1;j++){
+            if(arr[j]<arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+
+
+        }
+        
+    }
+    System.out.println("After Sorting"+Arrays.toString(arr));
+  }
+    
+}
