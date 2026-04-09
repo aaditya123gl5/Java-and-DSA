2df62b3e6474dd7a63cf5e8e0ee377bb7714ac79
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-04-09 15:58:11 +0530

    Removing whitespaces

---
+++ StringsandStringBuilder/Trim.java
@@ -0,0 +1,13 @@
+package StringsandStringBuilder;
+
+public class Trim {
+    public static void main(String[]args){
+        String str=" Ram and Shyam ";
+        //System.out.println(str);
+        //System.out.println(str.trim());
+        System.out.println(str.strip());
+
+    }
+
+    
+}
