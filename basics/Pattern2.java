53082b41080e3eb88b916a09f26a548733d2455f
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-04-11 17:30:38 +0530

    Inverted star pattern

--- basics/Pattern2.java
+++ basics/Pattern2.java
@@ -5,20 +5,11 @@ public class Pattern2 {
     public static void main(String[] args) {
     Scanner scn=new Scanner(System.in);
     int n=scn.nextInt();
-    int st=1;
-    int sp=n-1;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<sp;j++){
-            System.out.print("");
-        }
-         for (int j=0;j<st;j++){
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
             System.out.print("*");
         }
-
         System.out.println();
-        sp--;
-        st++;
-
     }
-}
+    }
 }
