e9e2fbd1a345003c35a9aaae4135f4ec042da8cd
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-04-12 17:48:12 +0530

    Pattern 3

---
+++ basics/Pattern3.java
@@ -0,0 +1,26 @@
+package basics;
+import java.util.*;
+
+public class Pattern3 {
+    public static void main(String[]args){
+    Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        
+        int stars = 1;
+        int spaces = n - 1;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= spaces;j++){
+                System.out.print("	");
+            }
+            for(int j = 1; j <= stars;j++){
+                System.out.print("*	");
+            }
+            
+            System.out.println();
+            stars++;
+            spaces--;
+    }
+    }
+}
