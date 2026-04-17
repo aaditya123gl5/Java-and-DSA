784850b0935a6c64e356ccd72685077ba3ef7459
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-04-17 23:18:41 +0530

    Modified

--- StringsandStringBuilder/IntegerToString.java
+++ StringsandStringBuilder/IntegerToString.java
@@ -4,7 +4,8 @@ public class IntegerToString {
     public static void main(String[]args){
     int x=235;
     int y=123;
-    System.out.println(Integer.toString(x)+Integer.toString(y));
+    int z=5;
+    System.out.println(Integer.toString(x)+Integer.toString(y)+Integer.toString(z));
     
  }
 }
