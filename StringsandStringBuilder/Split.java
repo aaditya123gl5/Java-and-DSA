c77108c7988eb0fd5658c4189a601ba97d6080a1
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-04-22 23:22:53 +0530

    split function

--- StringsandStringBuilder/Split.java
+++ StringsandStringBuilder/Split.java
@@ -2,7 +2,7 @@ package StringsandStringBuilder;
 
 public class Split {
    public static void main(String[]args){
-    String str="abc,def,ghi,jkl";
+    String str="abc,def,ghi,jkl,mno";
     String [] text=str.split(",");
     for(String t:text){
         System.out.println(t);
