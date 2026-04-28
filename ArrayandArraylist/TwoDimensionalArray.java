7189efb7be2e4f31cfc7e9c0b21506c9f2a6371b
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-04-28 20:00:04 +0530

    2-D array

---
+++ ArrayandArraylist/TwoDimensionalArray.java
@@ -0,0 +1,30 @@
+package ArrayandArraylist;
+
+import java.util.Scanner;
+
+public class TwoDimensionalArray {
+    public static void main(String[]args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int m = scn.nextInt();
+    int[][] arr = new int[n][m];
+    
+    for(int rows = 0; rows <= n-1; rows++)
+    {
+        for(int cols = 0; cols <= m-1; cols++)
+        {
+            arr[rows][cols] = scn.nextInt();
+        }
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+    }
+    }
+    
+}
