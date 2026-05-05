701099c7295599ce616623576629c7f548f66eae
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-05-05 07:47:39 +0530

    String to integer

--- StringsandStringBuilder/StringToInteger.java
+++ StringsandStringBuilder/StringToInteger.java
@@ -3,7 +3,7 @@ package StringsandStringBuilder;
 public class StringToInteger {
     public static void main(String[]args){
 
-    String s="1256";
+    String s="12567";
     System.out.println(Integer.parseInt(s));
     }
     
