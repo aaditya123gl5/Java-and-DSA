3efd53189b6b6355bf44a2355fae49c1b8c38fc0
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-05-10 19:53:01 +0530

    Implementation

---
+++ ArrayList/Implementation.java
@@ -0,0 +1,18 @@
+package Additional;
+import java.util.*;
+
+public class Implementation {
+    public static void main(String[] args) {
+    Scanner scn= new Scanner(System.in);
+    List<Integer> num=new ArrayList<>();
+    System.out.println("Enter number of elements");
+    int n=scn.nextInt();
+    for(int i=0;i<n;i++){
+        num.add(scn.nextInt());
+    }
+    System.out.println(num);
+    System.out.println("Enter element you want to search");
+    System.out.println(num.contains(scn.nextInt()));
+    scn.close();
+  }  
+}
