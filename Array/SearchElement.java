c15ec6d2e4f1426a5a9fa98ae0a3edd7fc218c9c
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-05-22 18:04:52 +0530

    Return index of element

---
+++ Array/SearchElement.java
@@ -0,0 +1,22 @@
+package Array;
+import java.util.*;
+public class SearchElement {
+    public static void main(String[]args){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=scn.nextInt();
+        }
+        System.out.println("Enter element you want to search");
+        int d=scn.nextInt();
+        int idx=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==d){
+                idx=i;
+            }
+        }
+        System.out.println(idx);
+
+    }
+}
