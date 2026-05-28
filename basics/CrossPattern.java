8b5471d787a884679ae098138a2907702c1bdae3
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-05-28 22:28:03 +0530

    Cross pattern

--- basics/CrossPattern.java
+++ basics/CrossPattern.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class CrossPattern {
     public static void main(String[]args){
+        System.out.println("Enter size of pattern");
         Scanner scn=new Scanner(System.in);
         int n=scn.nextInt();
         for(int i=1;i<=n;i++){
