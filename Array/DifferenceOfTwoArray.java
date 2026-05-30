80cb582ed5ff20845b33ae1ebd87c54ead473236
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-05-30 22:28:32 +0530

    Modified

--- Array/DifferenceOfTwoArray.java
+++ Array/DifferenceOfTwoArray.java
@@ -3,11 +3,13 @@ import java.util.*;
 public class DifferenceOfTwoArray {
     public static void main(String[]args){
       Scanner scn=new Scanner(System.in);
+      System.out.println("Enter length of array1");
       int n1=scn.nextInt();
       int[] a1=new int[n1];
       for(int i=0;i<a1.length;i++){
         a1[i]=scn.nextInt();
       }
+      System.out.println("Enter length of array2");
       int n2=scn.nextInt();
       int[] a2=new int[n2];
       for(int i=0;i<a2.length;i++){
