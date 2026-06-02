7e52bfa51097073b7e04da689beda84a50e9088c
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-06-02 22:07:14 +0530

    Removal of prime no. from list

--- ArrayList/RemovalOfPrimeNo.java
+++ ArrayList/RemovalOfPrimeNo.java
@@ -20,9 +20,10 @@ public class RemovalOfPrimeNo {
     }
     public static void main(String[]args){
         Scanner scn=new Scanner(System.in);
+        System.out.println("Enter size of list");
         int n=scn.nextInt();
         ArrayList<Integer> a1=new ArrayList<>();
-        System.out.println("Enter elements in arraylist");
+        System.out.println("Enter Elements in list");
         for(int i=0;i<n;i++){
             a1.add(scn.nextInt());
         }
