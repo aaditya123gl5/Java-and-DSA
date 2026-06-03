16a10a41bf283cee18a46bb0c7c25d57612d623d
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-06-03 17:19:32 +0530

    Modified

--- StringsandStringBuilder/ToggleCase.java
+++ StringsandStringBuilder/ToggleCase.java
@@ -18,6 +18,7 @@ public class ToggleCase {
     }
     public static void main(String[]args){
         Scanner scn=new Scanner(System.in);
+        System.out.println("Enter the String");
         String str=scn.nextLine();
         System.out.println(togglecase(str));
     }
