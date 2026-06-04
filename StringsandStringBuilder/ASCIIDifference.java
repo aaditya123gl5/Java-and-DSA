2e81803ec0287e69704a3fc95498564f429a43e5
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-06-04 21:53:57 +0530

    ASCII value difference between consecutive character

--- StringsandStringBuilder/ASCIIDifference.java
+++ StringsandStringBuilder/ASCIIDifference.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class ASCIIDifference {
     public static void main(String[]args){
         Scanner scn=new Scanner(System.in);
+        System.out.println("Enter string");
         String str=scn.nextLine();
         System.out.println(Solution(str));
     }
