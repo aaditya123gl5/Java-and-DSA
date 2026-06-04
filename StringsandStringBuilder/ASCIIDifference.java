7e2beea092951d0b61d57ff2b6706c6ec6d49af9
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-06-04 21:51:14 +0530

    Modified

---
+++ StringsandStringBuilder/ASCIIDifference.java
@@ -0,0 +1,21 @@
+package StringsandStringBuilder;
+import java.util.*;
+public class ASCIIDifference {
+    public static void main(String[]args){
+        Scanner scn=new Scanner(System.in);
+        String str=scn.nextLine();
+        System.out.println(Solution(str));
+    }
+    public static String Solution(String str){
+        StringBuilder sb=new StringBuilder();
+        sb.append(str.charAt(0)); 
+        for(int i=1;i<str.length();i++){
+            char curr=str.charAt(i);
+            char prev=str.charAt(i-1);
+            int gap=curr-prev;
+            sb.append(gap);
+            sb.append(curr);
+        }   
+        return sb.toString();
+    }
+}
