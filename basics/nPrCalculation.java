db6dfae1796a6cfd296a2731bed2340d15479c8f
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-06-10 20:59:05 +0530

    Calculation

---
+++ basics/nPrCalculation.java
@@ -0,0 +1,22 @@
+package basics;
+import java.util.*;
+
+public class nPrCalculation {
+    public static int fact(int x){
+        int rv=1;
+        for(int i=1;i<=x;i++){
+            rv=rv*i;
+        }
+        return rv;
+    }
+    public static void main(String[]args){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int r=scn.nextInt();
+        int nfact=fact(n);
+        int nmrfact=fact(n-r);
+        int nPr=nfact/nmrfact;
+        System.out.println(n+"P"+r+"="+nPr);
+    }
+}
+
