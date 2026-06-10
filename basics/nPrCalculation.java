8d30f1955c2ad4a77a67366e8a2699421559a1eb
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-06-10 21:23:07 +0530

    nPr Calculation

--- basics/nPrCalculation.java
+++ basics/nPrCalculation.java
@@ -11,7 +11,9 @@ public class nPrCalculation {
     }
     public static void main(String[]args){
         Scanner scn=new Scanner(System.in);
+        System.out.println("Enter value of n");
         int n=scn.nextInt();
+        System.out.println("Enter value of r");
         int r=scn.nextInt();
         int nfact=fact(n);
         int nmrfact=fact(n-r);
