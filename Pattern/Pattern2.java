eaf326c985d4d6fc1fe45fe233ba250d10154434
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-06-26 18:19:03 +0530

    Inverted Right angled triangle

--- Pattern/Pattern2.java
+++ Pattern/Pattern2.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Pattern2 {
     public static void main(String[] args) {
     Scanner scn=new Scanner(System.in);
+    System.out.println("Enter size of pattern");
     int n=scn.nextInt();
     for(int i=n;i>=1;i--){
         for(int j=1;j<=i;j++){
