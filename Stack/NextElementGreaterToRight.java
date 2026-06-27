10117d61a3c437cdd84d85fdb478bf2a923f686f
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-06-27 13:24:25 +0530

    Next greater element to right with O(n)

--- Stack/NextElementGreaterToRight.java
+++ Stack/NextElementGreaterToRight.java
@@ -7,12 +7,14 @@ public class NextElementGreaterToRight {
         for(int val:a){
             sb.append(val+"\n");
         }
-        System.out.println(sb);
+        System.out.println("Resultant array is\n"+sb);
     }
     public static void main(String[]args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter size of array");
         int n=Integer.parseInt(br.readLine());
         int[] a=new int[n];
+        System.out.println("Enter elements in array");
         for(int i=0;i<n;i++){
             a[i]=Integer.parseInt(br.readLine());
         }
