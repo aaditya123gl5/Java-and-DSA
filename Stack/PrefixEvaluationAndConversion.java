c6e76bc01b1780d29648cbf97f5b5205116de5b9
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-07-14 19:16:47 +0530

    Evaluation and convesion of prefix expression

--- Stack/PrefixEvaluationAndConversion.java
+++ Stack/PrefixEvaluationAndConversion.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class PrefixEvaluationAndConversion {
     public static void main(String[]args)throws Exception {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter prefix expression");
         String exp=br.readLine();
         
         Stack<Integer> vs=new Stack<>();
