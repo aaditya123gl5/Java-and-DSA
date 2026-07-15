a75f72b97e26f3eae3a884c8cc14260fd574c2da
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-07-15 21:24:49 +0530

    Span of array

--- Array/SpanOfArray.java
+++ Array/SpanOfArray.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class SpanOfArray {
     public static void main(String[]args){
         Scanner scn=new Scanner(System.in);
+        System.out.println("Enter size of array");
         int n=scn.nextInt();
         int[] arr= new int[n];
         for(int i=0;i<arr.length;i++){
