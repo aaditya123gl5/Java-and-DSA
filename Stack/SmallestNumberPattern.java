c727e0cad534deb6b29a988b3f289a61f3495178
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-07-18 12:14:49 +0530

    Smallest possible number

---
+++ Stack/SmallestNumberPattern.java
@@ -0,0 +1,32 @@
+package Stack;
+import java.util.*;
+import java.io.*;
+
+public class SmallestNumberPattern  {
+    public static void main(String[]args)throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String str=br.readLine();
+        Stack<Integer> st=new Stack<>();
+        int num=1;
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='d'){
+                st.push(num);
+                num++;
+            }
+            else{
+                st.push(num);
+                num++;
+                while(st.size()>0){
+                    System.out.print(st.pop());
+                }
+            }
+
+        }
+        st.push(num);
+        while(st.size()>0){
+            System.out.print(st.pop());
+        }
+    }
+    
+}
