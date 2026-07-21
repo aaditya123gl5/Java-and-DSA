584feb156bb7900d0303c45fae8721672f884b8a
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-07-21 22:56:36 +0530

    Inverse of Array

---
+++ Array/InverseOfArray.java
@@ -0,0 +1,26 @@
+package Additional;
+import java.io.*;
+import java.util.Arrays;
+
+public class InverseOfArray {
+    public static void main(String[]args)throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+    System.out.println(Arrays.toString(inverse(a)));
+    }
+    public static int[] inverse(int[]a){
+        int[] inv=new int[a.length];
+        for(int i=0;i<a.length;i++){
+            int v=a[i];
+            inv[v]=i;
+        }
+        return inv;
+       
+    }
+
+    
+}
