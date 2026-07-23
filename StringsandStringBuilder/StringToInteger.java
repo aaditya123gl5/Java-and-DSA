9cd2f6bfc173578c528d6debb45cf4b9dc99621c
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-07-23 19:29:37 +0530

    String to Integer without library function

--- StringsandStringBuilder/StringToInteger.java
+++ StringsandStringBuilder/StringToInteger.java
@@ -1,10 +1,19 @@
 package StringsandStringBuilder;
 
+import java.util.Scanner;
+
 public class StringToInteger {
     public static void main(String[]args){
-
-    String s="12567";
-    System.out.println(Integer.parseInt(s));
+    Scanner scn=new Scanner(System.in);
+    String s=scn.nextLine();
+    //System.out.println(Integer.parseInt(s));
+    int num=0;
+    int len=s.length();
+    for(int i=0;i<len;i++){
+        int currch=(int)(s.charAt(i));
+        num=num*10+(currch-48);
+    }
+    System.out.println(num);
     }
     
 }
