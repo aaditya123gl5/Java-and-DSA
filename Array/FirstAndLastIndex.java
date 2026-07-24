8f38b25f76fad98a142dda3f1e07c9d2f6e1ba03
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-07-24 22:42:57 +0530

    First and last index in sorted array

---
+++ Array/FirstAndLastIndex.java
@@ -0,0 +1,48 @@
+package Array;
+import java.util.*;
+
+public class FirstAndLastIndex {
+    public static void main(String[]args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    System.out.println("Enter sorted elements");
+    for(int i = 0; i < arr.length; i++){
+           arr[i] = scn.nextInt();
+    }
+    int data=scn.nextInt();
+    int lo=0;
+    int hi=arr.length-1;
+    int fi=-1;
+    while(lo<=hi){
+        int mid=(lo+hi)/2;
+        if(data>arr[mid]){
+            lo=mid+1;
+        }else if(data<arr[mid]){
+            hi=mid-1;
+        }else{
+            fi=mid;
+            hi=mid-1;
+        }
+    }
+   
+    lo=0;
+    hi=arr.length-1;
+    int li=-1;
+    while(lo<=hi){
+        int mid=(lo+hi)/2;
+        if(data>arr[mid]){
+            lo=mid+1;
+        }else if(data<arr[mid]){
+            hi=mid-1;
+        }else{
+            li=mid;
+            lo=mid+1;
+        }
+    }
+    System.out.println(fi);
+    System.out.println(li);
+    
+    }
+    
+}
