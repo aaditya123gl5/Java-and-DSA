ea76ff3f16bdc07b5b75441708c3346520da982c
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-07-24 22:44:14 +0530

    Binary Search

---
+++ Array/BinarySeach.java
@@ -0,0 +1,28 @@
+package Additional;
+import java.util.*;
+
+public class BinarySeach {
+    public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0; i < arr.length; i++){
+           arr[i] = scn.nextInt();
+    }
+    int data=scn.nextInt();
+    int l= 0;
+    int h = arr.length - 1;
+    while(l <= h){
+        int m = (l+ h) / 2;
+        if(data > arr[m]){
+           l= m + 1;
+        } else if(data < arr[m]){
+           h = m -1;
+        } else {
+            System.out.println(m);
+            return;
+        }
+    }
+     System.out.println(-1);
+   }
+} 
