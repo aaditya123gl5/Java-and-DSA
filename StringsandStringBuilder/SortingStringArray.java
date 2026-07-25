1ced2477b4a707de9aca29c18dd547f58cadbf22
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-07-25 21:11:48 +0530

    Sorting string array

---
+++ StringsandStringBuilder/SortingStringArray.java
@@ -0,0 +1,15 @@
+package Additional;
+import java.util.*;
+
+public class SortingStringArray {
+    public static void main(String[] args){
+        String[] str=new String[3];
+        str[0]="ram";
+        str[1]="abc";
+        str[2]="lmn";
+        System.out.println(Arrays.toString(str));
+
+
+    }
+    
+}
