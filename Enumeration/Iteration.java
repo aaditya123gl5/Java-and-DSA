adc66be123d5ca539d19719d930ec74b7855e2d4
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-08-05 21:34:07 +0530

    Iterating over enum values

---
+++ Enumeration/Iteration.java
@@ -0,0 +1,15 @@
+package Enumeration;
+enum Color{
+    RED, GREEN, BLUE;
+    
+}
+public class Iteration{
+    
+    public static void main(String[] args){
+        
+        for (Color c : Color.values()){
+            
+            System.out.println(c);
+        }
+    }
+}
