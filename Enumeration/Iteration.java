13e0a351a7caf7b5b2138e524937f95b87dc5887
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-08-05 21:37:11 +0530

    Iterating over enums values

--- Enumeration/Iteration.java
+++ Enumeration/Iteration.java
@@ -6,7 +6,7 @@ enum Color{
 public class Iteration{
     
     public static void main(String[] args){
-        
+        //values()-> return an array of all constant inside enums
         for (Color c : Color.values()){
             
             System.out.println(c);
