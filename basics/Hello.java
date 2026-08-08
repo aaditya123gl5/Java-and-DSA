e8ef1bdc270c0029f4192304886a6cbb5e9970a2
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-08-08 23:23:10 +0530

    Hello in java

--- basics/Hello.java
+++ basics/Hello.java
@@ -2,7 +2,9 @@ package basics;
 
 public class Hello {
     public static void main(String[]args){
-        System.out.println("Hello world");
+       System.out.println("Hello"); 
+
     }
+       
     
 }
