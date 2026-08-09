4d229cc7a1b3583878f58e28824c83a2b4f97caf
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-08-09 22:32:59 +0530

    Basic function of queue

---
+++ Queues/basics.java
@@ -0,0 +1,17 @@
+package Queues;
+import java.util.*;
+
+public class basics {
+    public static void main(String[]args){
+        Queue<Integer> q=new ArrayDeque<>();
+        q.add(10);
+        System.out.println(q);
+        q.add(20);
+        System.out.println(q);
+        q.add(30);
+        System.out.println(q);
+
+    }
+                
+    
+}
