dced8e3889924e013e8e6721ce5ffad87d43713a
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-08-12 21:43:47 +0530

    Next element to right

--- Stack/NextElementSmallerToRight.java
+++ Stack/NextElementSmallerToRight.java
@@ -14,9 +14,10 @@ public class NextElementSmallerToRight {
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    System.out.println("Enter size of array");
     int n = Integer.parseInt(br.readLine());
     int[] a = new int[n];
+    System.out.println("Enter array elements");
     for (int i = 0; i < n; i++) {
       a[i] = Integer.parseInt(br.readLine());
     }
