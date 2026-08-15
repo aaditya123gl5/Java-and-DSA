f774f12c00dbcf874a6b488d25903ef9270c2124
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-08-15 17:23:49 +0530

    Benjamin bulb problem

---
+++ basics/BenjaminBulb.java
@@ -0,0 +1,15 @@
+package basics;
+
+import java.util.Scanner;
+
+public class BenjaminBulb {
+    public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    
+    for(int i = 1; i * i <= n; i++){
+        System.out.println(i*i);
+    }
+   }
+    
+}
