8faad699b17a95eead04f4ef09c72b96afe5cf33
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-08-20 23:44:56 +0530

    Local Class

---
+++ NestedClasses/LocalClass.java
@@ -0,0 +1,23 @@
+package NestedClasses;
+
+public class LocalClass {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.greet();
+    }
+}
+
+// Effective final variable
+
+class Outer {
+    void greet() {
+        int y = 5;
+            class Local {
+            void sayHello() {
+                System.out.println(y);
+            }
+        }
+        Local local  = new Local();
+        local.sayHello();
+    }
+}
