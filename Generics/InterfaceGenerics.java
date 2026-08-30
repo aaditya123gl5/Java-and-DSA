66498ff96e278b2d11129aaca3a6983dccaa7069
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-08-30 20:28:45 +0530

    Interface Generics

---
+++ Generics/InterfaceGenerics.java
@@ -0,0 +1,32 @@
+package Generics;
+
+public class InterfaceGenerics {
+    public static void main(String[] args) {
+        Box<Fish> b1 = new Box<>();
+    }
+}
+
+class Box<T extends Animal & Swimmable> {
+    T value;
+}
+
+class Animal {
+    void display() {
+        System.out.println("Displaying Animal");
+    }
+}
+
+interface Swimmable {
+    void swim();
+}
+
+class Dog extends Animal {
+
+}
+
+class Fish extends Animal implements Swimmable {
+    @Override
+    public void swim() {
+        System.out.println("Fish is swimming");
+    }
+}
