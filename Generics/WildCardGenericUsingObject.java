7b363ab796a05df71f80ec3183ec71272f27b5e1
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-09-02 22:20:30 +0530

    Wildcard generic using object class

---
+++ Generics/WildCardGenericUsingObject.java
@@ -0,0 +1,49 @@
+package Generics;
+import java.util.List;
+import java.util.ArrayList;
+
+public class WildCardGenericUsingObject {
+     public static void main(String[] args) {
+        List<Dog> dogs = new ArrayList<>();
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        fun(dogs);
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal());
+        animals.add(new Animal());
+        fun(animals);
+        
+    }
+
+    static void fun(List<?> values) {
+       
+        for(Object obj: values) {
+         //System.out.println(obj.getClass().getName());//class name with Package
+        System.out.println(obj.getClass().getSimpleName());//Only class name
+    }
+
+   
+    }
+}
+
+class Animal {
+    void eat() {
+        System.out.println("Animal Eating");
+    }
+
+    void walk() {
+        System.out.println("Walking");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void eat() {
+        System.out.println("Dog Eating");
+    }
+
+    void bark() {
+        System.out.println("Barking");
+    }
+}
+
