66b0b59249889b3bd5fcf2e3bd3555bc87395493
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-09-06 17:33:13 +0530

    List methods

---
+++ JCF/ListMethods.java
@@ -0,0 +1,76 @@
+package JCF;
+
+public class ListMethods {
+
+    // 1. add(E e) - Adds an element at the end.
+    // list.add(1);
+
+
+    // 2. add(int index, E e) - Adds an element at the specified index.
+    // list.add(0, 5);
+
+
+    // 3. addAll(Collection<? extends E> c) - Adds all elements of another collection.
+    // list.addAll(List.of(2, 3, 4));
+
+
+    // 4. addAll(int index, Collection<? extends E> c) - Adds elements from given index.
+    // list.addAll(0, List.of(9, 8, 7));
+
+
+    // 5. get(int index) - Returns the element at the specified index.
+    // list.get(1);
+
+
+    // 6. set(int index, E e) - Replaces the element at the specified index.
+    // list.set(1, 5);
+
+
+    // 7. remove(int index) - Removes the element at the specified index.
+    // list.remove(0);
+
+
+    // 8. remove(Object o) - Removes the specified element.
+    // list.remove(Integer.valueOf(5));
+
+
+    // 9. indexOf(Object o) - Returns the first index of the element.
+    // list.indexOf(2);
+
+
+    // 10. lastIndexOf(Object o) - Returns the last index of the element.
+    // list.lastIndexOf(2);
+
+
+    // 11. listIterator() - Returns a ListIterator for traversing the list.
+    // list.listIterator();
+
+
+    // 12. listIterator(int index) - Returns a ListIterator from given index.
+    // list.listIterator(2);
+
+
+    // 13. subList(int fromIndex, int toIndex) - Returns a portion of the list.
+    // list.subList(1, 3);
+
+
+    // 14. replaceAll(UnaryOperator<E>) - Replaces each element using the given operation.
+    // list.replaceAll(x -> x * 2);
+
+
+    // 15. sort(Comparator<? super E>) - Sorts the list using the given comparator.
+    // list.sort(Comparator.naturalOrder());
+
+
+    // 16. reversed() - Returns a reversed view of the list. (Java 21+)
+    // list.reversed();
+
+
+    // 17. List.of(E...) - Creates an immutable list.
+    // List<Integer> list = List.of(1, 2, 3);
+
+
+    // 18. List.copyOf(Collection) - Creates an immutable copy of a collection.
+    // List<Integer> list2 = List.copyOf(list);
+
+}
