287b2be6c90abdbba1519b43af1bc212c15c0e7d
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-09-14 22:52:39 +0530

    Intersection of two array

---
+++ Array/IntersectionOfTwoArray.java
@@ -0,0 +1,20 @@
+package Array;
+import java.util.*;
+
+public class IntersectionOfTwoArray {
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        int[] arr1={1,2,3,4,5,5,6};
+        int[] arr2={4,5,6,7,7,8};
+        Set<Integer> set1=new HashSet<>();
+        Set<Integer> set2=new HashSet<>();
+        for(int word:arr1){
+            set1.add(word);
+        }
+        for(int word2:arr2){
+            set2.add(word2);
+        }
+        set1.retainAll(set2);
+        System.out.println(set1);
+    }
+}
