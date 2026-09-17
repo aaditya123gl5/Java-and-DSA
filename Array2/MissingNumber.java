f9f43e0c43b9de4f807c061f696f07acfb166444
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-09-17 21:24:41 +0530

    Missing number

--- Array2/MissingNumber.java
+++ Array2/MissingNumber.java
@@ -2,21 +2,17 @@ package Array2;
 import java.util.*;
 
 public class MissingNumber {
-    public static void main(String[] args){
-        Scanner scn=new Scanner(System.in);
-        int n=scn.nextInt();
-        int[] arr=new int[n];
-        Set<Integer> set=new HashSet<>();
-        for(int i=0;i<n;i++){
-            arr[i]=scn.nextInt();
+    public static void main(String[] args) {
+       Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(scn.nextInt());
         }
-        for(int word:arr){
-            set.add(word);
-        }
-        for(int i=1;i<=n+1;i++){
-            if(!set.contains(i)){
-               System.out.println(i);
-               break;
+        for (int i = 1; i <= n + 1; i++) {
+            if (!set.contains(i)) {
+                System.out.println("Missing number: " + i);
+                break;
             }
         }
     }
