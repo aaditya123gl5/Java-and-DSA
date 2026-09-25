d74b33ab93235fc58a2d4acd9d287d581bf41ba8
Java-and-Dsa <aaditya123gl5@gmail.com>
2026-09-25 22:15:33 +0530

    Queue Method

--- Queues/QueueMethod.java
+++ Queues/QueueMethod.java
@@ -6,23 +6,21 @@ public class QueueMethod {
     
     Queue<Integer> queue = new ArrayDeque<>();
         // ADD / ENQUEUE
-        queue.add(10);
-        queue.offer(20);
+        queue.add(10);            // Adds element, throws exception if failed
+        queue.offer(20);          // Adds element, returns false if failed (Thread safe)
         queue.add(30);
         queue.offer(40);
 
         // ACCESS FRONT
-        System.out.println(queue.element());
-        System.out.println(queue.peek());
-
+        System.out.println(queue.element());  // Returns front, exception if empty
+        System.out.println(queue.peek());     // Returns front, null if empty (Thread safe)
 
         // REMOVE FRONT
-        System.out.println(queue.remove());
-        System.out.println(queue.poll());
+        System.out.println(queue.remove());    // Removes front, exception if empty
+        System.out.println(queue.poll());      // Removes front, null if empty (Thread Safe)
 
         // SIZE
         System.out.println(queue.size());
-
 
 
         // CHECK EMPTY
@@ -36,23 +34,18 @@ public class QueueMethod {
         // REMOVE SPECIFIC ELEMENT
         queue.remove(30);
 
-
-
         // ADD MULTIPLE ELEMENTS
         queue.addAll(Arrays.asList(50, 60, 70));
 
 
-  
         // CHECK COMMON ELEMENTS
         System.out.println(queue.containsAll(Arrays.asList(50, 60)));
 
 
-      
         // REMOVE MULTIPLE ELEMENTS
         queue.removeAll(Arrays.asList(50, 60));
 
 
-    
         // RETAIN COMMON ELEMENTS
         queue.retainAll(Arrays.asList(20, 40));
 
@@ -65,11 +58,10 @@ public class QueueMethod {
             System.out.println(x);
         }
 
-
-        
         // CONVERT TO ARRAY
         Object[] arr = queue.toArray();
 
         Integer[] arr2 = queue.toArray(new Integer[0]);
     }
 }
+
